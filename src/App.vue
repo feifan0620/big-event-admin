22c7d87efc95e53ece4f83d0d891b3d7c8847bb9
feifan0620 <feifan0620@qq.com>
2024-09-28 21:42:26 +0800

    feat(dependencies): 添加element-plus和unplugin依赖
    在项目依赖中添加了element-plus和来自unplugin的多个插件，以增强UI组件和自动导入功能。

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 
 <template>
   <div>大事件管理系统</div>
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
 <style scoped></style>
