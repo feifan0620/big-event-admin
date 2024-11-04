c7e24a6679d03c2fcac94cfed1c1d3a603121b80
“feifan0620” <feifan0620@qq.com>
2024-11-05 02:30:02 +0800

    feat(article): 添加文章分类和文章管理页面的框架
    - 在 ArticleChannelView 和 ArticleManageView 中引入 PageContainer 组件
    - 为文章分类和文章管理页面添加标题和操作按钮
    - 优化页面布局，提高用户体验

--- src/views/article/ArticleChannelView.vue
+++ src/views/article/ArticleChannelView.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 <template>
-  <div>文章频道管理</div>
+  <page-container title="文章分类">
+    <template #extra>
+      <el-button type="primary"> 添加分类 </el-button>
+    </template>
+  </page-container>
 </template>
 <style scoped lang="scss"></style>
--- src/views/article/ArticleManageView.vue
+++ src/views/article/ArticleManageView.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 <template>
-  <div>文章管理</div>
+  <page-container title="文章管理">
+    <template #extra>
+      <el-button type="primary">发布文章</el-button>
+    </template>
+  </page-container>
 </template>
 <style scoped lang="scss"></style>
